a960f86ca74d7533072f611dfb42bdec0565c374
aldaMarMu <alda.martin@bq.com>
2018-12-20 13:24:18 +0100

    User schema working

---
+++ src/models/userModel.ts
@@ -0,0 +1,55 @@
+import * as mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const ContactSchema = new Schema({
+  id: Schema.Types.ObjectId,
+
+  email: {
+    type: String,
+    unique: true,
+    required: 'Please enter your email',
+    trim: true,
+    lowercase: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  name: {
+    type: String,
+    trim: true,
+  },
+
+  center: {
+    type: String,
+    trim: true,
+  },
+
+  active: {
+    type: Boolean,
+  },
+
+  signUpToken: {
+    type: Number,
+  },
+
+  authToken: {
+    type: Number,
+  },
+
+  notifications: {
+    type: Boolean,
+  },
+
+  created_date: {
+    type: Date,
+  },
+});
+
+
+var UserMong = mongoose.model('UserMongs', ContactSchema);
+//module.exports = UserMong;
+export {UserMong};
